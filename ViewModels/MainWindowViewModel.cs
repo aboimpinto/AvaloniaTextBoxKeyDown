760dd32a2ffb5bcad603be2a18ec24133cfc6790
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-06-25 00:57:25 +0200

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Reactive;
+using System.Windows.Input;
+using Avalonia.Input;
+using ReactiveUI;
+
+namespace AvaloniaTextBoxKeyDown.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public ICommand KeyPressCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.KeyPressCommand = ReactiveCommand.Create<KeyEventArgs, Unit>(this.OnKeyPress);
+        }
+
+        private Unit OnKeyPress(KeyEventArgs arg)
+        {
+            if (arg.Key == Key.Enter)
+            {
+                // do what you need to do
+            }
+
+            return Unit.Default;
+        }
+    }
+}
